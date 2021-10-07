97e81d8cfa43f058f9cbf7bbbb4c78614eef3cf5
tedo74 <teddy2@abv.bg>
2021-10-07 12:20:10 +0300

    abstract factory

---
+++ creational/factory/abstractFactory.ts
@@ -0,0 +1,95 @@
+interface IMotherboard {
+  mbName: string;
+  platform: string;
+}
+
+interface ICpu {
+  cpuName: string;
+  platform: string;
+}
+
+class AsusDarkHero implements IMotherboard {
+  mbName: string;
+  platform: string;
+  constructor() {
+    this.mbName = 'AsusDarkHero';
+    this.platform = 'AMD';
+  }
+}
+
+class MsiMeg implements IMotherboard {
+  mbName: string;
+  platform: string;
+  constructor() {
+    this.mbName = 'MsiMeg';
+    this.platform = 'Intel';
+  }
+}
+
+class Ryzen implements ICpu {
+  cpuName: string;
+  platform: string;
+  constructor() {
+    this.cpuName = 'Ryzen';
+    this.platform = 'AMD';
+  }
+}
+
+class IntelI9 implements ICpu {
+  cpuName: string;
+  platform: string;
+  constructor() {
+    this.cpuName = 'I9';
+    this.platform = 'Intel';
+  }
+}
+
+interface IFactory {
+  getCpu(): ICpu;
+  getMotherboard(): IMotherboard;
+}
+
+// families of related objects
+class AmdFactory implements IFactory {
+  getCpu(): ICpu {
+    return new Ryzen();
+  }
+  getMotherboard(): IMotherboard {
+    return new AsusDarkHero();
+  }
+}
+
+class IntelFactory implements IFactory {
+  getCpu(): ICpu {
+    return new IntelI9();
+  }
+  getMotherboard(): IMotherboard {
+    return new MsiMeg();
+  }
+}
+
+interface IComputer {
+  cpu: ICpu;
+  motherboard: IMotherboard;
+}
+
+class MainFactory {
+  static assemble(platform: string): IComputer | undefined {
+    if (platform === 'AMD') {
+      const amdFactory = new AmdFactory();
+      const cpu = amdFactory.getCpu();
+      const motherboard = amdFactory.getMotherboard();
+      return { cpu, motherboard };
+    } else if (platform === 'Intel') {
+      const intelFactory = new IntelFactory();
+      const cpu = intelFactory.getCpu();
+      const motherboard = intelFactory.getMotherboard();
+      return { cpu, motherboard };
+    } else {
+      return undefined;
+    }
+  }
+}
+
+console.log(MainFactory.assemble('AMD'));
+console.log(MainFactory.assemble('Intel'));
--- dist/creational/factory/abstractFactory.js
+++ dist/creational/factory/abstractFactory.js
@@ -1,1 +1,63 @@
 "use strict";
+class AsusDarkHero {
+    constructor() {
+        this.mbName = 'AsusDarkHero';
+        this.platform = 'AMD';
+    }
+}
+class MsiMeg {
+    constructor() {
+        this.mbName = 'MsiMeg';
+        this.platform = 'Intel';
+    }
+}
+class Ryzen {
+    constructor() {
+        this.cpuName = 'Ryzen';
+        this.platform = 'AMD';
+    }
+}
+class IntelI9 {
+    constructor() {
+        this.cpuName = 'I9';
+        this.platform = 'Intel';
+    }
+}
+// families of related objects
+class AmdFactory {
+    getCpu() {
+        return new Ryzen();
+    }
+    getMotherboard() {
+        return new AsusDarkHero();
+    }
+}
+class IntelFactory {
+    getCpu() {
+        return new IntelI9();
+    }
+    getMotherboard() {
+        return new MsiMeg();
+    }
+}
+class MainFactory {
+    static assemble(platform) {
+        if (platform === 'AMD') {
+            const amdFactory = new AmdFactory();
+            const cpu = amdFactory.getCpu();
+            const motherboard = amdFactory.getMotherboard();
+            return { cpu, motherboard };
+        }
+        else if (platform === 'Intel') {
+            const intelFactory = new IntelFactory();
+            const cpu = intelFactory.getCpu();
+            const motherboard = intelFactory.getMotherboard();
+            return { cpu, motherboard };
+        }
+        else {
+            return undefined;
+        }
+    }
+}
+console.log(MainFactory.assemble('AMD'));
+console.log(MainFactory.assemble('Intel'));
