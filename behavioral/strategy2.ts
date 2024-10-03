77ebf39f53d3e50c29cb7be30ceab0304411278d
teddy <teddy2@abv.bg>
2024-10-03 14:11:55 +0300

    Strategy 2

---
+++ behavioral/strategy2.ts
@@ -0,0 +1,38 @@
+interface Payment {
+    pay(amount: number): void;
+}
+
+class PayPal implements Payment {
+    pay(amount: number): void {
+        console.log(`Pay ${amount} using PayPal`);
+    }
+}
+
+class creditCard implements Payment {
+    pay(amount: number): void {
+        console.log(`Pay ${amount} using credit card`);
+    }
+}
+
+class ShoppingCart {
+    private amount = 0;
+
+    constructor(private payStrategy: Payment) {}
+
+    public setPaymentStrategy(strategy: Payment): void {
+        this.payStrategy = strategy;
+    }
+    
+    public addToCart(value: number): void {
+        this.amount = value;
+    }
+
+    public checkOut(): void {
+        this.payStrategy.pay(this.amount);
+        this.amount = 0;
+    }
+}
+
+const cart = new ShoppingCart(new PayPal());
+cart.addToCart(150);
+cart.checkOut();
